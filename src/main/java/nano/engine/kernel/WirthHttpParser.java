3f02405f3507a51bb6cdcdfa4433f3aa9d1c5d98
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-23 17:37:30 +0300

    Wirth wip 2

--- src/main/java/nano/engine/kernel/WirthHttpParser.java
+++ src/main/java/nano/engine/kernel/WirthHttpParser.java
@@ -1,6 +1,7 @@
 package nano.engine.kernel;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 
 
 public class WirthHttpParser{
@@ -17,18 +18,49 @@ public class WirthHttpParser{
 	private final int value;
 	public int value() {return value;}
     }
+    public enum METHODS{
+	GET("GET"),
+	HEAD("HEAD"),
+	POST("POST"),
+	PUT("GET");
+	METHODS(String value){this.value=value;}
+	private final String value;
+	public String value() {return value;}
+	public byte[] bValue() {return value.getBytes();}
+    }
     
     public void  parse(ByteArrayOutputStream stream){
 	var payload = stream.toByteArray();
 	var status = STATUS.REQ_START;
-	var current_byte = 0;
+	//	var current_byte = 0;
+	var current_word = new byte[4];
 	for (int i=0; i < payload.length; i++){
-	    current_byte = payload[i];
+	    //  current_byte = payload[i];
+	    
 	    switch(status){
 	    case STATUS.REQ_START: {
+		//no spaces allowed here must be POST,GET and so on
+		//jump to 
+		status = STATUS.REQ_METHOD;
+		current_word[i] = payload[i];
 		
 	    };
+		break;
 	    case STATUS.REQ_METHOD: {
+		if(current_word.length < 3){
+		    status = STATUS.REQ_METHOD;
+		    current_word[i] = payload[i];
+		    break;
+		}
+		if(current_word.length==3){
+		    var method = isGetOrPut(current_word);
+		    if(method!=null){
+			//do something
+		    }else {
+			//other 
+		    }
+			
+		}
 		
 	    };
 	    case STATUS.REQ_URI: {
@@ -55,6 +87,15 @@ public class WirthHttpParser{
 	    
 	}
 	
+    }
+    private METHODS isGetOrPut(byte[] word){
+	if(Arrays.equals(word, METHODS.GET.bValue())){
+	    return METHODS.GET;
+	}
+	else if(Arrays.equals(word, METHODS.PUT.bValue())){
+	    return METHODS.PUT;
+	}
+	return null;
     }
 
 }
